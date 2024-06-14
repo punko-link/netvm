8f250eb999544d2e151c54cbafe44bc061cf3526
glabSdr <glabstrizhkov@gmail.com>
2024-06-15 00:51:37 +0300

    handle_connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 mod master;
 
 fn main() {
-    let a: u8 = 5;
-    if a == true as u8 {
-        println!("A")
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+        println!("Connection established!");
     }
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+    let buf_reader = BufReader::new(&mut stream);
+    let incoming: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take(65535)
+        .collect();
 }
